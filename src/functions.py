fcd2ec0e4678fde6f4f442975fa74fc4966a00bc
Joseph <josephryan96@gmail.com>
2019-05-09 19:48:26 -0700

    functions.py complete

--- src/functions.py
+++ src/functions.py
@@ -3,6 +3,9 @@
 # YOUR CODE HERE
 
 
+def is_even(i):
+    if i % 2 == 0:
+        return True
 
 
 # Read a number from the keyboard
@@ -12,3 +15,13 @@ num = int(num)
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
 # YOUR CODE HERE
+
+
+def num_check(num):
+    if ((num % 2) == 0):
+        print("Even!")
+    else:
+        print("Odd")
+
+
+num_check(num)

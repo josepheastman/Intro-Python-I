b14d6eac7f37fe2698b61931f29e2b79df3c208e
Joseph <josephryan96@gmail.com>
2019-05-09 20:33:48 -0700

    args.py complete

--- src/args.py
+++ src/args.py
@@ -6,6 +6,11 @@
 
 # YOUR CODE HERE
 
+
+def f1(arg1, arg2):
+    return arg1 + arg2
+
+
 print(f1(1, 2))
 
 # Write a function f2 that takes any number of integer arguments and prints the
@@ -14,6 +19,11 @@ print(f1(1, 2))
 # YOUR CODE HERE
 
 
+def f2(*args):
+    sum = 0
+    for i in args:
+        sum += i
+    return sum
 
 
 print(f2(1))                    # Should print 1
@@ -24,7 +34,7 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 a = [7, 6, 5, 4]
 
 # What thing do you have to add to make this work?
-  print(f2(a))   # Should print 22
+print(f2(*a))   # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
@@ -32,6 +42,12 @@ a = [7, 6, 5, 4]
 
 # YOUR CODE HERE
 
+
+def f3(arg1, arg2=0):
+    if arg2 == 0:
+        return arg1 + 1
+    else:
+        return arg1 + arg2
 
 
 print(f3(1, 2))  # Should print 3
@@ -47,6 +63,9 @@ print(f3(8))     # Should print 9
 # Google "python keyword arguments".
 
 # YOUR CODE HERE
+def f4(**kwargs):
+    for key, value in kwargs.items():
+        print(f"key: {key}, value: {value}".format(key, value))
 
 
 # Should print
@@ -66,4 +85,4 @@ d = {
 }
 
 # What thing do you have to add to make this work?
-f4(d)
+f4(**d)
